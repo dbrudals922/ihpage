06a377729a4657aae16a330b1cb53d39b0702eb5
유경민 <>
2024-04-13 14:58:51 +0900

    no message

--- ih-frontend/src/main.js
+++ ih-frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./index.js";
 import store from "./store";
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
