da7193c708bcb71ce5a6423fdc0ed0a3ec1abd47
유경민 <>
2024-04-14 10:18:53 +0900

    no message

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://port-0-backend-85phb42bluxo83i2.sel5.cloudtype.app//api/auth/';
-const API_URL3 = 'https://port-0-backend-85phb42bluxo83i2.sel5.cloudtype.app//api/info/';
+const API_URL = 'https://port-0-backend-85phb42bluxo83i2.sel5.cloudtype.app/api/auth/';
+const API_URL3 = 'https://port-0-backend-85phb42bluxo83i2.sel5.cloudtype.app/api/info/';
 
 class AuthService {
   login(user) {
