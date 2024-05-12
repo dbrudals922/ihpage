a797b66926059dfcbc4d49af82d2c0f30398bbad
유경민 <>
2024-05-12 18:05:31 +0900

    no message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -49,7 +49,7 @@ export default defineConfig({
     server: {
       proxy: {
         '/api': {
-          target: 'https://localhost:8080',
+          target: 'http://localhost:8080',
           changeOrigin: true,
           // rewrite: (path) => path.replace(/^\/api/, ''),
           secure: false,
